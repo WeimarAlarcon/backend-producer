48d3196f2cb422a3a7e2dd7e6a67da9ba3ac31a9
Weimar Alarcon Reynaga <weimaralarcon55@gmail.com>
2024-09-27 00:00:04 -0400

    proyecto base

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalPipes(new ValidationPipe());
+  app.setGlobalPrefix('api'); 
+  app.enableCors();
+  await app.listen(process.env.APP_PORT);
+  console.log(`Application is running on: http://localhost:${ process.env.APP_PORT }`);
 }
 bootstrap();
